5c515f74158bcc09cca6cf3770b3d5f0bfc207f1
U-WIN81CuWiredCPU\D1g1Ts <edward.mauthe201@myci.csuci.edu>
2020-08-27 20:12:19 -0700

    ruby http html server example

--- b.rb
+++ b.rb
@@ -5,9 +5,27 @@ server=TCPServer.new port
 loop do
 	client=server.accept
 	puts client.gets
-	#client.puts "Hi"
-	#client.puts "GET /\r\n"
-	client.puts "<a href=\"/\">test</a>"
+	# body
+	b=""
+	b+="<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">"
+	b+="<HEAD>"
+	b+="<TITLE>This</TITLE>"
+	b+="</HEAD>"
+	b+="<BODY>"
+	b+="<h1>Document</h1>"
+	b+="<p>Explanation"
+	b+="<p>More stuff"
+	b+="</BODY>"
+	# header
+	h=""
+	h+="HTTP/1.0 200\n"
+	h+="Content-Type: text/html\n"
+	# the reason why the content length need +1 was
+	#  that client.puts was being used which put a newline
+	#  after the body, hence the extra character
+	h+="Content-Length: %d\n"%[b.length]
+	h+="\n"
+	client.print h+b
 	#client.puts ""
 	client.close
 end
